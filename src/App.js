1bb8449ca0d61a3d6218619521c8fd3c93186238
whilini <whi0306@naver.com>
2022-02-18 00:46:16 +0900

    Theme Provider-black&white모드 만들기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,53 +10,13 @@ const Wrapper = styled.div`
   justify-content: center;
   height: 100vh;
   width: 100vw;
-`;
-
-const rotationAnimation = keyframes`
-0% {
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-50% {
-  transform: rotate(180deg);
-  border-radius: 100px;
-}
-100% {
-  transform: rotate(360deg);
-  border-radius: 0px;
-}
-`;
-
-const Emogi = styled.span`
-  font-size: 50px;
-  &:hover {
-    font-size: 100px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: lightskyblue;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  &:hover {
-    background-color: pink;
-  }
-  ${Emogi}:active {
-    opacity: 0;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const App = () => {
   return (
     <Wrapper>
-      <Box>
-        <Emogi as='a'>💕</Emogi>
-      </Box>
-      <Emogi as='a'>💞</Emogi>
+      <Title>Hello</Title>
     </Wrapper>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#232526",
+};
+
+const lightTheme = {
+  textColor: "#232526",
+  backgroundColor: "whitesmoke",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
